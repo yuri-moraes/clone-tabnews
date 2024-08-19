d4625398c5a5753294f3c57dca9abab17b29404b
Yuri Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-08-19 19:26:17 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um programa para aprender de fora a fora um projeto completo.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 export default function Home() {
-    return (
-        <h1>Test</h1>
-    )
+  return <h1>Test</h1>;
+}
+
+function test() {
+  return null;
 }
