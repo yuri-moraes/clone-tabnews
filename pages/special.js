b8daa699a26ed759e05507d0ea1c88a32ad0c423
Yuri Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-08-19 15:26:27 +0000

    adding pages

---
+++ pages/special.js
@@ -0,0 +1,9 @@
+export default function special() {
+    return (
+        <>
+            <h1>Oi gata!</h1>
+            <p>Saiba que você é uma pessoa bem especial para mim</p>
+            <p>Te amo!</p>
+        </>
+    )
+}
